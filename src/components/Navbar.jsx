200d6f387eec8db710ea0b01aa87400f082725b6
Zenkqi <SSGSSAchita@gmail.com>
2024-10-06 09:37:57 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,14 +59,9 @@ function Navbar() {
                             </Link>
                         </li>
 
-                        <li className='nav-item'>
-                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                                Sign Up
-                            </Link>
-                        </li>
                     </ul>
 
-                    {button  && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {button  && <Button buttonStyle='btn--outline'>LEARN MORE</Button>}
 
                 </div>
             </nav>
